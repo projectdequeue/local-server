823aaa522caaa3cb3f3f6dca516b43f1aa41c534
Sumod <sumod@sumod-desktop.(none)>
2013-03-29 08:51:18 +0530

    new app added

---
+++ projectdequeue/hall/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+from film.models import Film
+from hall.models import Hall
+
+def buy_ticket(request):
+	filmid=request.GET.get('filmid','')
+	tickets=long(request.POST['number_of_tickets'])	
+
+	selected_film=Film.objects.get(film_id__exact=filmid)
+	hall_id=selected_film.hall_id
+	
+	film_hall=Hall.objects.get(hall_id__exact=hall_id)
+	total_seats=film_hall.total_seats	
+	seats_allocated=film_hall.total_seats_allocated
+
+	seats_available=total_seats - seats_allocated
+	amount_payable=tickets*selected_film.ticket_charge
+	if(seats_available>tickets):
+		film_hall.total_seats_allocated +=tickets
+		film_hall.save()
+		return render_to_response('buy_ticket.html',{'amount':amount_payable,"selected_film": Film.objects.filter(film_id__exact=filmid) },context_instance=RequestContext(request))	
